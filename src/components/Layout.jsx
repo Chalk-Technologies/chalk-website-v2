caec2f017e10c083892c24cc72c99831883f8150
Aloys Berger <aloysberger@gmail.com>
2021-05-17 13:47:03 +0200

    fix: :bug: Fix overflow on the main screen (add overflow hidden to layout)

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,9 +4,9 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden">
       <Navbar />
-      <main className="bg-chalk-dark-gray flex flex-col flex-grow relative">
+      <main className="relative flex flex-col flex-grow bg-chalk-dark-gray">
         {children}
       </main>
       <Footer />
