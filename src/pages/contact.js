dfbcd7ccb32496a9401dd9366a72ad4825dd13ee
Aloys Berger <aloysberger@gmail.com>
2021-05-17 13:37:00 +0200

    style: :lipstick: Scaffold the contact page for API call
    Add the first name and last name input, make it responsive.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,82 +1,116 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { useForm } from "react-hook-form";
 
 import { Link } from "gatsby";
 import Layout from "../components/Layout";
 
 const Contact = () => {
   const [status, setStatus] = useState("initial");
+  const { register, handleSubmit } = useForm();
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = (data) => {
     setStatus("loading");
 
-    try {
-      //SEND EMAIL HERE
-      setStatus("success");
-    } catch (err) {
-      console.error(err);
-      setStatus("error");
-    }
+    const { email, first, last, phone } = data;
+
+    let body = JSON.stringify({
+      email,
+      first,
+      phone,
+      last,
+    });
+
+    fetch("/", {
+      method: "POST",
+      body,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+      })
+      .then(() => {
+        setStatus("success");
+      })
+      .catch((err) => {
+        console.error(err);
+        setStatus("error");
+      });
   };
 
   return (
     <Layout>
-      <section className="flex-grow flex flex-col pt-36">
+      <section className="flex flex-col flex-grow pt-36">
         <section className="">
-          <h1 className="pb-12 text-5xl text-white uppercase font-heading pl-8">
+          <h1 className="pb-12 pl-8 text-5xl text-white uppercase font-heading">
             Contact <br />
             Us
           </h1>
           <div className="relative">
-            <Image className="bg-gyms bg-cover py-96 bg-center" />
-            <div className="bg-chalk-orange w-1 h-full absolute left-0 top-0 ml-24 mt-24 z-10" />
+            <Image className="bg-center bg-cover bg-gyms py-96" />
+            <div className="absolute top-0 left-0 z-10 w-1 h-full mt-24 ml-24 bg-chalk-orange" />
           </div>
         </section>
-        <section className="absolute min-h-screen w-screen top-0 left-0 flex justify-center align-center pt-80 px-2 lg:py-80 z-30">
+        <section className="absolute top-0 left-0 z-30 flex justify-center w-screen min-h-screen px-2 mt-20 align-center pt-60 lg:py-60">
           <form
-            onSubmit={handleSubmit}
-            className="container mx-auto rounded-lg shadow-lg flex flex-col items-stretch justify-around p-8 px-4 lg:px-16 bg-chalk-dark-gray mx-24 border-2 border-chalk-orange"
+            onSubmit={handleSubmit(onSubmit)}
+            className="container flex flex-col items-stretch justify-around p-8 px-4 mx-24 mx-auto border-2 rounded-lg shadow-lg lg:px-16 bg-chalk-dark-gray border-chalk-orange"
           >
             {status === "initial" && (
-              <h1 className="text-white text-4xl uppercase font-heading flex justify-center items-center my-3">
+              <h1 className="flex items-center justify-center my-3 text-4xl text-white uppercase font-heading">
                 Send us an email
               </h1>
             )}
             {status === "initial" && (
-              <div className="flex-grow flex flex-col">
+              <div className="flex flex-col flex-grow">
                 <input
+                  {...register("title")}
                   type="text"
                   required
                   placeholder="Subject"
-                  className="my-3 flex flex-start items-start px-4 py-2 font-heading text-chalk-dark-gray"
+                  className="flex items-start px-4 py-2 my-3 flex-start font-heading text-chalk-dark-gray"
                 />
                 <textarea
+                  {...register("message")}
                   type="text"
                   required
                   placeholder="Message"
-                  className="my-3 flex-grow flex flex-start items-start py-2 px-4 font-heading text-chalk-dark-gray"
+                  className="flex items-start flex-grow px-4 py-2 my-3 flex-start font-heading text-chalk-dark-gray"
                   rows={10}
                 />
                 <input
+                  {...register("email")}
                   type="email"
                   required
                   placeholder="your@email.com"
-                  className="py-2 px-4 font-heading text-chalk-dark-gray my-3"
+                  className="px-4 py-2 my-3 font-heading text-chalk-dark-gray"
                 />
-                <button className="px-8 py-4 uppercase font-heading text-white border-white border-2 hover:text-chalk-dark-gray hover:bg-chalk-orange hover:border-chalk-orange transition w-1/2 mx-auto my-4 mt-6">
+                <div className="flex flex-wrap flex-grow">
+                  <input
+                    {...register("first")}
+                    placeholder="First name"
+                    className="flex-grow px-4 py-2 mb-3 font-heading text-chalk-gray lg:mb-0 lg:mr-2"
+                  />
+                  <input
+                    {...register("last")}
+                    placeholder="Last name"
+                    className="flex-grow px-4 py-2 font-heading text-chalk-gray lg:ml-2"
+                  />
+                </div>
+                <button className="w-1/2 px-8 py-4 mx-auto my-4 mt-6 text-white uppercase transition border-2 border-white font-heading hover:text-chalk-dark-gray hover:bg-chalk-orange hover:border-chalk-orange">
                   Send
                 </button>
               </div>
             )}
             {status === "success" && (
-              <div className="flex-grow flex flex-col items-center justify-center">
-                <h2 className="font-body text-2xl flex justify-center items-center text-white font-heading">
+              <div className="flex flex-col items-center justify-center flex-grow">
+                <h2 className="flex items-center justify-center text-2xl text-white font-body font-heading">
                   ✅ All good! We'll get back to you as soon as we're finished
                   climbing.
                 </h2>
                 <Link
-                  className="px-8 py-4 border-white border-2 font-heading mx-auto my-4 hover:border-chalk-orange hover:bg-chalk-orange transition text-white uppercase mt-12"
+                  className="px-8 py-4 mx-auto my-4 mt-12 text-white uppercase transition border-2 border-white font-heading hover:border-chalk-orange hover:bg-chalk-orange"
                   to="/"
                 >
                   Back home
@@ -84,15 +118,15 @@ const Contact = () => {
               </div>
             )}
             {status === "error" && (
-              <div className="flex-grow flex flex-col items-center justify-center">
-                <h2 className="font-body text-2xl flex justify-center items-center text-white font-heading">
+              <div className="flex flex-col items-center justify-center flex-grow">
+                <h2 className="flex items-center justify-center text-2xl text-white font-body font-heading">
                   😕 Something went wrong...
                 </h2>
-                <h2 className="font-body text-2xl flex justify-center items-center text-white font-heading mt-2">
+                <h2 className="flex items-center justify-center mt-2 text-2xl text-white font-body font-heading">
                   Alternatively you can send us a good ol' fashioned email by
                   clicking{" "}
                   <a
-                    className="font-bold text-chalk-orange ml-2"
+                    className="ml-2 font-bold text-chalk-orange"
                     href="mailto:info@chalk-technologies.com"
                     target="_blank"
                   >
@@ -101,7 +135,7 @@ const Contact = () => {
                   .
                 </h2>
                 <Link
-                  className="px-8 py-4 border-white border-2 font-heading mx-auto my-4 hover:border-chalk-orange hover:bg-chalk-orange transition text-white uppercase mt-12"
+                  className="px-8 py-4 mx-auto my-4 mt-12 text-white uppercase transition border-2 border-white font-heading hover:border-chalk-orange hover:bg-chalk-orange"
                   to="/"
                 >
                   Back home
